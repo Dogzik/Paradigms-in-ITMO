9be54da8b64b16136164daed7e453fa03b798404
Dogzik <levdovzhik@gmail.com>
2017-05-24 17:14:37 +0300

    Hard version added

--- HW12/expression.clj
+++ HW12/expression.clj
@@ -4,20 +4,25 @@
       )
   )
 
+(defn constant [value] (fn [vars] value))
+(defn variable [name] (fn [vars] (vars name)))
+
 (def add (operator +))
 (def subtract (operator -))
 (def multiply (operator *))
-(def divide (operator (fn [x y] (try (/ x y) (catch Exception e (/ x 0.0))))))
+(def divide (operator (fn [x y] (/ (double x) (double y)))))
 (def negate (operator -))
+
 (def sin (operator (fn [x] (Math/sin x))))
 (def cos (operator (fn [x] (Math/cos x))))
 (def sinh (operator (fn [x] (Math/sinh x))))
 (def cosh (operator (fn [x] (Math/cosh x))))
-(def sqrt (operator (fn [x] (Math/sqrt x))))
+(def sqrt (operator (fn [x] (Math/sqrt (Math/abs x)))))
 (def square (operator (fn [x] (* x x))))
 
 (defn constant [value] (fn [vars] value))
 (defn variable [name] (fn [vars] (get vars name)))
+
 (def op {'+ add, '- subtract, '* multiply, '/ divide,
          'negate negate, 'sin sin, 'cos cos, 'sinh sinh,
          'cosh cosh, 'sqrt sqrt, 'square square
@@ -25,7 +30,7 @@
 (defn parseFunction [expression]
      (cond
         (string? expression) (parseFunction (read-string expression))
-        (seq? expression) (apply (get op (first expression)) (map parseFunction (rest expression)))
+        (seq? expression) (apply (op (first expression)) (map parseFunction (rest expression)))
         (number? expression) (constant expression)
         (symbol? expression) (variable (str expression))
         )
